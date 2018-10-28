7b42f73ebe88f1cc405fa57dcaa173611afb1ded
baytoor <ms.ms.1@bk.ru>
2018-10-29 02:42:02 +0600

    Initial Commit

---
+++ TNP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TNP
+//
+//  Created by baytoor on 10/29/18.
+//  Copyright © 2018 alish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
